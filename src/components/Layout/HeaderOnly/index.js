265e4c8d626fdd284600903486e174043fb5544a
Vu Thanh Cong <congvt@3tit.vn>
2025-02-22 18:22:55 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,17 @@
+import Header from '~/components/Layout/components/Header';
+
+function HeaderOnly({ children }) {
+    return (  
+        <div>
+            <Header />
+            <div className='container'>
+                <div className='content'>
+                    { children }
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default HeaderOnly;
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [ // dont need auth
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = []; // need auth
+
+export { publicRoutes, privateRoutes }
