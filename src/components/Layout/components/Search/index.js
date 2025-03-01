8c14862499f8fc5af9d6c585cecad77e0480299e
Vu Thanh Cong <congvt@3tit.vn>
2025-03-01 17:08:08 +0700

    Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -1,13 +1,14 @@
-import classNames from 'classnames/bind';
-import styles from './Search.module.scss';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import AccountItem from '~/components/AccountItem';
-import { faSpinner, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import { SearchIcon } from '~/components/Icons';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState, useRef } from 'react';
 import HeadlessTippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import { useEffect, useRef, useState } from 'react';
+import AccountItem from '~/components/AccountItem';
+import { SearchIcon } from '~/components/Icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { useDebounce } from '~/hooks';
+import styles from './Search.module.scss';
+import * as searchService from '~/apiServices/searchServices';
 
 const cx = classNames.bind(styles);
 
@@ -27,17 +28,17 @@ function Search() {
             return;
         }
         
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then(res => res.json())
-            .then(res => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            })
+            const result = await searchService.search(debounced);
+            setSearchResult(result);
+
+            setLoading(false);
+        }
+
+        fetchApi();
+        
     }, [debounced]);
 
     const handleClear = () => {
