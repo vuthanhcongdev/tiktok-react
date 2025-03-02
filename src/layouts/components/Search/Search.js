55a9b362af12aad7cf113faff31a64b6bd5b67fb
Vu Thanh Cong <congvt@3tit.vn>
2025-03-02 09:16:56 +0700

    Tái cấu trúc và tối ưu code #3

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -15,7 +15,7 @@ const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const debounced = useDebounce(searchValue, 500);
